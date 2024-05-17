a15f0470302b2e62a5b79827fbac3a41be26aab1
goreleaserbot <goreleaser@carlosbecker.com>
2024-05-17 21:30:23 +0300

    Brew formula update for bert version v2.4.1

--- Formula/bert.rb
+++ Formula/bert.rb
@@ -5,13 +5,13 @@
 class Bert < Formula
   desc "CLI benchmarking tool"
   homepage "https://sha1n.github.io/bert/"
-  version "2.4.0"
+  version "2.4.1"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/sha1n/bert/releases/download/v2.4.0/bert_2.4.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "643916b897fac4483a48bdb39bfbb86f24120375ca37308e33695d3ffb2f0f04"
+    on_intel do
+      url "https://github.com/sha1n/bert/releases/download/v2.4.1/bert_2.4.1_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "4f96e1c8c2a4f589d1669657825f6d9306d8040f793aa7b57de55c3a21b762ce"
 
       def install
         bin.install "bert"
@@ -20,9 +20,9 @@ class Bert < Formula
         zsh_completion.install "completions/_bert" => "_bert"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/sha1n/bert/releases/download/v2.4.0/bert_2.4.0_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "c8009f549517101968e1f8804192d4d8add0c53d66f173ff9d80a3089fb06ed1"
+    on_arm do
+      url "https://github.com/sha1n/bert/releases/download/v2.4.1/bert_2.4.1_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "c2f5fe4e752702aa1a1afd4dcc5654d275333f09183fa86424329a8b5cad4d6d"
 
       def install
         bin.install "bert"
@@ -34,37 +34,43 @@ class Bert < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/sha1n/bert/releases/download/v2.4.0/bert_2.4.0_linux_armv6.tar.gz", using: CurlDownloadStrategy
-      sha256 "0e3d987f74cd007351327078bba618296806590577a3fc89c118f0d84154e7ee"
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/sha1n/bert/releases/download/v2.4.1/bert_2.4.1_linux_amd64.tar.gz", using: CurlDownloadStrategy
+        sha256 "cd3d4d36094ae0089a2b89fce247186f8bf408f7b58d09d09147d191a8eca404"
 
-      def install
-        bin.install "bert"
-        bash_completion.install "completions/bert.bash" => "bert"
-        fish_completion.install "completions/bert.fish" => "bert"
-        zsh_completion.install "completions/_bert" => "_bert"
+        def install
+          bin.install "bert"
+          bash_completion.install "completions/bert.bash" => "bert"
+          fish_completion.install "completions/bert.fish" => "bert"
+          zsh_completion.install "completions/_bert" => "_bert"
+        end
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sha1n/bert/releases/download/v2.4.0/bert_2.4.0_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "6902b98748eff7aa0607b2c4ed5da1ed1b1447ab96f5be1c256a4d21519c366f"
+    on_arm do
+      if !Hardware::CPU.is_64_bit?
+        url "https://github.com/sha1n/bert/releases/download/v2.4.1/bert_2.4.1_linux_armv6.tar.gz", using: CurlDownloadStrategy
+        sha256 "336bd90b77b0984d691648eee34c41dd830a46669a932dfc044567effb45c8bf"
 
-      def install
-        bin.install "bert"
-        bash_completion.install "completions/bert.bash" => "bert"
-        fish_completion.install "completions/bert.fish" => "bert"
-        zsh_completion.install "completions/_bert" => "_bert"
+        def install
+          bin.install "bert"
+          bash_completion.install "completions/bert.bash" => "bert"
+          fish_completion.install "completions/bert.fish" => "bert"
+          zsh_completion.install "completions/_bert" => "_bert"
+        end
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/sha1n/bert/releases/download/v2.4.0/bert_2.4.0_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "6f232319331454ab2affc3a3b8039dee70505d3ff44458f6fdc4cce653e8d060"
+    on_arm do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/sha1n/bert/releases/download/v2.4.1/bert_2.4.1_linux_arm64.tar.gz", using: CurlDownloadStrategy
+        sha256 "ca4a68b9993384fcef763799ff0dfebb28efad9129059c619aeef87637221c61"
 
-      def install
-        bin.install "bert"
-        bash_completion.install "completions/bert.bash" => "bert"
-        fish_completion.install "completions/bert.fish" => "bert"
-        zsh_completion.install "completions/_bert" => "_bert"
+        def install
+          bin.install "bert"
+          bash_completion.install "completions/bert.bash" => "bert"
+          fish_completion.install "completions/bert.fish" => "bert"
+          zsh_completion.install "completions/_bert" => "_bert"
+        end
       end
     end
   end
